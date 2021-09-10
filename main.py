75e9736bf9b1097a4a2520f2adb05bcac52bc45b
Paul Curran <paulcurranit@gmail.com>
2021-09-10 16:56:31 +0100

    Updated formatting.

--- main.py
+++ main.py
@@ -70,69 +70,71 @@ teamDictionary = {
     "Deportivo Alavés": "Alaves",
     "Athletic Club": "Ath Bilbao",
     "Club Atlético de Madrid": "Ath Madrid",
-    "FC Barcelona" : "Barcelona",
-    "Real Betis Balompié" : "Betis",
-    "Cádiz CF" : "Cadiz",
-    "RC Celta de Vigo" : "Celta",
-    "Elche CF" : "Elche",
+    "FC Barcelona": "Barcelona",
+    "Real Betis Balompié": "Betis",
+    "Cádiz CF": "Cadiz",
+    "RC Celta de Vigo": "Celta",
+    "Elche CF": "Elche",
     "RCD Espanyol de Barcelona": "Espanol",
-    "Getafe CF" : "Getafe",
-    "Granada CF" : "Granada",
-    "Levante UD" : "Levante",
+    "Getafe CF": "Getafe",
+    "Granada CF": "Granada",
+    "Levante UD": "Levante",
     "RCD Mallorca": "Mallorca",
-    "CA Osasuna" : "Osasuna",
-    "Real Madrid CF" : "Real Madrid",
-    "Sevilla FC" : "Sevilla",
-    "Real Sociedad de Fútbol" : "Sociedad",
-    "Valencia CF" : "Valencia",
+    "CA Osasuna": "Osasuna",
+    "Real Madrid CF": "Real Madrid",
+    "Sevilla FC": "Sevilla",
+    "Real Sociedad de Fútbol": "Sociedad",
+    "Valencia CF": "Valencia",
     "Rayo Vallecano de Madrid": "Vallecano",
-    "Villarreal CF" : "Villarreal",
+    "Villarreal CF": "Villarreal",
     # Serie A
-    "Atalanta BC" : "Atalanta",
-    "Bologna FC 1909" : "Bologna",
-    "Cagliari Calcio" : "Cagliari",
-    "Empoli FC" : "Empoli",
-    "ACF Fiorentina" : "Fiorentina",
-    "Genoa CFC" : "Genoa",
-    "FC Internazionale Milano" : "Inter",
-    "Juventus FC" : "Juventus",
-    "SS Lazio" : "Lazio",
-    "AC Milan" : "Milan",
+    "Atalanta BC": "Atalanta",
+    "Bologna FC 1909": "Bologna",
+    "Cagliari Calcio": "Cagliari",
+    "Empoli FC": "Empoli",
+    "ACF Fiorentina": "Fiorentina",
+    "Genoa CFC": "Genoa",
+    "FC Internazionale Milano": "Inter",
+    "Juventus FC": "Juventus",
+    "SS Lazio": "Lazio",
+    "AC Milan": "Milan",
     "SSC Napoli": "Napoli",
-    "AS Roma" : "Roma",
-    "US Salernitana 1919" : "Salernitana",
-    "UC Sampdoria" : "Sampdoria",
-    "US Sassuolo Calcio" : "Sassuolo",
-    "Spezia Calcio" : "Spezia",
-    "Torino FC" : "Torino",
-    "Udinese Calcio" : "Udinese",
-    "Venezia FC" : "Venezia",
-    "Hellas Verona FC" : "Verona"
+    "AS Roma": "Roma",
+    "US Salernitana 1919": "Salernitana",
+    "UC Sampdoria": "Sampdoria",
+    "US Sassuolo Calcio": "Sassuolo",
+    "Spezia Calcio": "Spezia",
+    "Torino FC": "Torino",
+    "Udinese Calcio": "Udinese",
+    "Venezia FC": "Venezia",
+    "Hellas Verona FC": "Verona"
 }
 
+
 def downloadstats(url):
-    return pd.read_csv(url, usecols=['Date', 'Time', 'HomeTeam', 'AwayTeam','FTHG','FTAG'])
+    return pd.read_csv(url, usecols=['Date', 'Time', 'HomeTeam', 'AwayTeam', 'FTHG', 'FTAG'])
 
 
-def downloadfixures(league, dateFrom, dateTo):
-    xAuthToken = os.environ.get('FootballPredictor')
-    headers_dict = {"X-Auth-Token": xAuthToken}
-    apiCall = "https://api.football-data.org/v2/competitions/" + league + "/matches?dateFrom=" + dateFrom + "&dateTo=" + dateTo
+def downloadfixures(league, date_from, date_to):
+    x_auth_token = os.environ.get('FootballPredictor')
+    headers_dict = {"X-Auth-Token": x_auth_token}
+    api_call = "https://api.football-data.org/v2/competitions/" + league + "/matches?dateFrom=" + date_from + "&dateTo=" + date_to
     response = json.loads(
-                json.dumps(requests.get(
-        apiCall,
-        headers=headers_dict
-    ).json()))
+                json.dumps(
+                    requests.get(
+                        api_call,
+                        headers=headers_dict
+                    ).json()))
 
     matches = response["matches"]
-    formattedMatches = []
+    formatted_matches = []
     for match in matches:
-        formattedMatches.append([teamDictionary[match["homeTeam"]["name"]], teamDictionary[match["awayTeam"]["name"]]])
+        formatted_matches.append([teamDictionary[match["homeTeam"]["name"]], teamDictionary[match["awayTeam"]["name"]]])
 
-    return formattedMatches
+    return formatted_matches
 
 
-def getCoefficient(url):
+def get_coefficient(url):
     df = downloadstats(url)
     df['goal_difference'] = df['FTHG'] - df['FTAG']
 
@@ -152,24 +154,24 @@ def getCoefficient(url):
     df_train = df_model  # setting up a training object equal to the df_model, not necessary
 
     lr = Ridge(alpha=0.001)
-    X = df_train.drop(['goal_difference'], axis=1)
+    x = df_train.drop(['goal_difference'], axis=1)
     y = df_train['goal_difference']
 
-    lr.fit(X, y)
+    lr.fit(x, y)
 
-    df_ratings = pd.DataFrame(data={'team': X.columns, 'rating': lr.coef_})
+    df_ratings = pd.DataFrame(data={'team': x.columns, 'rating': lr.coef_})
     return df_ratings
 
 
-def getCoefByTeam(team, ratings):
+def get_coefficient_by_team(team, ratings):
     for index, row in ratings.iterrows():
         if row["team"] == team:
             return row["rating"]
 
 
 def predict(home, away, ratings):
-    home_coef = getCoefByTeam(home, ratings)
-    away_coef = getCoefByTeam(away, ratings)
+    home_coef = get_coefficient_by_team(home, ratings)
+    away_coef = get_coefficient_by_team(away, ratings)
 
     if home_coef >= away_coef:
         return ["home win", home, home_coef - away_coef]
@@ -177,7 +179,7 @@ def predict(home, away, ratings):
         return ["away win", away, away_coef - home_coef]
 
 
-def predictWeekendFixtures(games, ratings):
+def predict_weekend_fixtures(games, ratings):
     results = []
     for game in games:
         results.append(predict(game[0], game[1], ratings))
@@ -185,12 +187,12 @@ def predictWeekendFixtures(games, ratings):
     return results
 
 
-def printResults(results):
+def print_results(results):
     print()
     print("Results")
     print("========")
     for result in results:
-        print(result[0] + " " + result[1] + " by: " + str(round(result[2],2)))
+        print(result[0] + " " + result[1] + " by: " + str(round(result[2], 2)))
 
 
 if __name__ == '__main__':
@@ -200,11 +202,11 @@ if __name__ == '__main__':
     BL_STATS = "https://www.football-data.co.uk/mmz4281/2122/D1.csv"
     FL_STATS = "https://www.football-data.co.uk/mmz4281/2122/F1.csv"
 
-    pl_ratings = getCoefficient(PREMIERLEAGUE_STATS)
-    ll_ratings = getCoefficient(LALIGA_STATS)
-    sa_ratings = getCoefficient(SA_STATS)
-    bl_ratings = getCoefficient(BL_STATS)
-    fl_ratings = getCoefficient(FL_STATS)
+    pl_ratings = get_coefficient(PREMIERLEAGUE_STATS)
+    ll_ratings = get_coefficient(LALIGA_STATS)
+    sa_ratings = get_coefficient(SA_STATS)
+    bl_ratings = get_coefficient(BL_STATS)
+    fl_ratings = get_coefficient(FL_STATS)
 
     dateFrom = "2021-09-10"
     dateTo = "2021-09-14"
@@ -213,23 +215,23 @@ if __name__ == '__main__':
 
     print(dateFrom + " to " + dateTo)
     pl_games = downloadfixures("PL", dateFrom, dateTo)
-    pl_results = predictWeekendFixtures(pl_games, pl_ratings)
-    printResults(pl_results)
+    pl_results = predict_weekend_fixtures(pl_games, pl_ratings)
+    print_results(pl_results)
 
     ll_games = downloadfixures("PD", dateFrom, dateTo)
-    ll_results = predictWeekendFixtures(ll_games, ll_ratings)
-    printResults(ll_results)
+    ll_results = predict_weekend_fixtures(ll_games, ll_ratings)
+    print_results(ll_results)
 
     sa_games = downloadfixures("SA", dateFrom, dateTo)
-    sa_results = predictWeekendFixtures(sa_games, sa_ratings)
-    printResults(sa_results)
+    sa_results = predict_weekend_fixtures(sa_games, sa_ratings)
+    print_results(sa_results)
 
     bl_games = downloadfixures("BL1", dateFrom, dateTo)
-    bl_results = predictWeekendFixtures(bl_games, bl_ratings)
-    printResults(bl_results)
+    bl_results = predict_weekend_fixtures(bl_games, bl_ratings)
+    print_results(bl_results)
 
     fl_games = downloadfixures("FL1", dateFrom, dateTo)
-    fl_results = predictWeekendFixtures(fl_games, fl_ratings)
-    printResults(fl_results)
+    fl_results = predict_weekend_fixtures(fl_games, fl_ratings)
+    print_results(fl_results)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
